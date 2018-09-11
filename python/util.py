e8db8778e49de2cfd7ecb82715032bc3f0a1d8c1
Yuguang Tong <ygtong@berkeley.edu>
2018-09-11 11:04:48 -0700

    more test on the drifting kappa modification. clean up branch

--- python/util.py
+++ python/util.py
@@ -59,6 +59,51 @@ kappa_in = {kappa_in}
 '''
     return string
 
+def gen_input_file_text(input_params):
+    scalar_keys = ['kstart', 'kend', 'nk', 'omega_r', 'omega_i', 'increment_r', 'increment_i', 'nspecies',
+            'theta', 'delta'
+           ]
+    vector_keys = ['q_in', 'mu_in', 'dens_in', 'drift_in', 'beta_para_in', 'beta_perp_in', 'kappa_in']
+    for key in scalar_keys:
+        assert key in input_params.keys()
+    for key in vector_keys:
+        assert key in input_params.keys()
+        assert input_params['nspecies'] == len(input_params[key])
+    
+    input_file_text = ''
+    input_file_text += get_wave_number(
+        input_params['kstart'], 
+        input_params['kend'], 
+        input_params['nk']
+    )
+    input_file_text += get_initial_guess(
+        omega_r=input_params['omega_r'], 
+        omega_i=input_params['omega_i'], 
+        increment_r=input_params['increment_r'],
+        increment_i=input_params['increment_i']
+    )
+    input_file_text += get_setup(
+        nspecies=input_params['nspecies'], 
+        theta=input_params['theta'], 
+        delta=input_params['delta']
+    )
+    input_file_text += get_accuracy(
+        rf_error=1e-4, 
+        eps_error=1e-6
+    )
+
+    for j in range(len(input_params['q_in'])):
+        input_file_text += get_species(
+            q_in=input_params['q_in'][j],
+            mu_in=input_params['mu_in'][j],
+            dens_in=input_params['dens_in'][j],
+            drift_in=input_params['drift_in'][j],
+            beta_para_in=input_params['beta_para_in'][j],
+            beta_perp_in=input_params['beta_perp_in'][j],
+            kappa_in=input_params['kappa_in'][j],
+        )    
+    return input_file_text
+
 def write_input_data(input_file_text, input_file_name="input.dat"):
     with open(input_file_name,"w") as f:
         f.write(input_file_text)
